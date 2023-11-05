0e7d39f497b26c49774834f4cbe425c70091cb63
Robert Nyström <robert.nystrom@toolsense.io>
2023-11-05 13:28:13 +0100

    refactor main with pickled network

--- __main__.py
+++ __main__.py
@@ -1,13 +1,27 @@
 import os
+from time import perf_counter
 
 from dotenv import load_dotenv
+from loguru import logger
 
 from philips_hue_v2.bridge import HueBridge
-from philips_hue_v2.resource.network import Network
+from philips_hue_v2.resource.network import Network, pickle_network, unpickle_network
 from philips_hue_v2.resource.requests import get_resources
 
 
 load_dotenv()  # take environment variables from .env.
+
+
+def main_load_pickled_network() -> None:
+    """Main entry point when using a pickled network."""
+    network = unpickle_network()
+    bibblan = network.get_light_by_id("23e8c74f-7c0e-40ae-b61d-f10df2f165be")
+
+    if not bibblan:
+        return
+
+    bibblan.turn_on()
+    bibblan.set_rgb_color({"red": 255, "green": 255, "blue": 255})
 
 
 def main() -> None:
@@ -18,7 +32,9 @@ def main() -> None:
         user_name=os.getenv("USER_NAME", ""),
     )
     resources_response = get_resources(bridge)
-    network = Network(resources=resources_response.unwrap(), bridge=bridge)
+    resources = resources_response.unwrap()
+    network = Network(resources=resources, bridge=bridge)
+
     bibblan = network.get_light_by_id("23e8c74f-7c0e-40ae-b61d-f10df2f165be")
 
     if not bibblan:
@@ -29,4 +45,6 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    start = perf_counter()
+    main_load_pickled_network()
+    logger.info(perf_counter() - start)
